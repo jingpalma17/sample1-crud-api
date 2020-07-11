493027f1ac33ea10e7a2b32cb7c6e78aee3a405f
Jing Palma <jingpalma17@gmail.com>
2020-07-11 16:26:45 +0800

    Add swagger UI for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Sample1 CRUD')
+    .setDescription('The Sample1 CRUD API documentation')
+    .setVersion('1.0')
+    // .addTag('tag')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
