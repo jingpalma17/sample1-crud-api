e7933f9d1d609469175a6629ce86801c380dd271
Jing Palma <jingpalma17@gmail.com>
2020-07-11 18:22:09 +0800

    Change global path; Add config folder (Initial commit)

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const basePath = '/api'; // Use config setting ./config
+  const explorerPath = 'explorer';
+
+  app.setGlobalPrefix(basePath);
   const options = new DocumentBuilder()
+    .setBasePath(basePath)
     .setTitle('Sample1 CRUD')
     .setDescription('The Sample1 CRUD API documentation')
     .setVersion('1.0')
     // .addTag('tag')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup(explorerPath, app, document);
 
   await app.listen(3000);
 }
